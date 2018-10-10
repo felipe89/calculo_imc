273256c454125d53d7bdb0b34b1fb72ca4a4e339
Gilmar Silva <gilmar.silva@reservafacil.tur.br>
2018-10-10 14:49:10 -0300

    correção pontual

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ void main () {
        weightController.text = "";
        heightController.text = "";
        _infoText = "Informe seu peso e altura";
+     }
 
        void _calculate() {
          setState(() {
@@ -32,26 +33,21 @@ void main () {
 
            if (imc < 18.6) {
              _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+           } else if (imc >= 18.6 && imc < 24.9) {
+             _infoText = "Peso ideal (${imc.toStringAsPrecision(4)})";
+           } else if (imc >= 24.9 && imc < 29.9) {
+             _infoText =
+             "Um pouco acima do peso (${imc.toStringAsPrecision(4)})";
+           } else if (imc >= 29.9 && imc < 34.9) {
+             _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+           } else if (imc >= 34.9 && imc < 39.9) {
+             _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+           } else if (imc >= 40) {
+             _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+           }
+         });
+       }
 
-           }else if (imc >=18.6 && imc < 24.9){
-           _infoText = "Peso ideal (${imc.toStringAsPrecision(4)})";
-
-         }  else if (imc >=24.9 && imc < 29.9){
-           _infoText = "Um pouco acima do peso (${imc.toStringAsPrecision(4)})";
-
-         }else if (imc >=29.9 && imc < 34.9){
-         _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
-
-         }else if (imc >=34.9 && imc < 39.9){
-         _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
-
-         }else if (imc >= 40)
-         {
-         _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
-         }
-       });
-     }
-  }
        @override
        Widget build(BuildContext context) {
          return Scaffold(
